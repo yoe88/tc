35c2520797670f0c0c0fa5895ddeeac8d1ecf589
yohan <hijr88@naver.com>
2023-03-22 15:22:07 +0900

    15552 - 빠른 입출력 예제

---
+++ src/main/java/me/yh/basic/Main_15552.java
@@ -0,0 +1,23 @@
+package me.yh.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/15552
+public class Main_15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
