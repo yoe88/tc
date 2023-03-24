16835510d45dff7a3c5a43ab6a059a0a885e22f2
yohan <hijr88@naver.com>
2023-03-24 21:19:36 +0900

    2440 - 별찍기 3

---
+++ src/main/java/me/yh/star/Main_2440.java
@@ -0,0 +1,22 @@
+package me.yh.star;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//https://www.acmicpc.net/problem/2440
+public class Main_2440 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
